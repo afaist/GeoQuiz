f14f614c072243ca0b76141271b028f6eda73922
afaist <afaist@gmail.com>
2022-12-06 18:31:29 +0700

    Chapter 2

--- app/src/main/java/com/afaist/geoquiz/MainActivity.kt
+++ app/src/main/java/com/afaist/geoquiz/MainActivity.kt
@@ -2,30 +2,65 @@ package com.afaist.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf<Question>(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        trueButton.setOnClickListener {
-            val snackBar = Snackbar.make(it, R.string.correct_message, Snackbar.LENGTH_SHORT)
-            snackBar.setTextColor(ContextCompat.getColor(this, R.color.white))
-            snackBar.setBackgroundTint(ContextCompat.getColor(this, R.color.blue))
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
-            snackBar.show()
+        trueButton.setOnClickListener {
+           checkAnswer(true, it)
         }
         falseButton.setOnClickListener {
-            val snackBar = Snackbar.make(it, R.string.incorrect_message, Snackbar.LENGTH_SHORT)
-            snackBar.setTextColor(ContextCompat.getColor(this, R.color.white))
-            snackBar.setBackgroundTint(ContextCompat.getColor(this, R.color.red))
-            snackBar.show()
+           checkAnswer(false, it)
         }
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean, view: View) {
+        val correctAnswer = questionBank[currentIndex].answer
+        var messageResId = R.string.correct_message
+        var color = ContextCompat.getColor(this, R.color.blue)
+        if (userAnswer != correctAnswer) {
+            messageResId = R.string.incorrect_message
+            color = ContextCompat.getColor(this, R.color.red)
+        }
+        val snackBar: Snackbar = Snackbar.make(view, messageResId, Snackbar.LENGTH_SHORT)
+        snackBar.setBackgroundTint(color)
+        snackBar.show()
     }
 }
